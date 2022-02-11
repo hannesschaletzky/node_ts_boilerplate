e1c16a61be912ea701e3258334b7656bf29335c2
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2022-02-11 12:44:42 +0100

    added http server creation

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,45 @@
 // let the linter complain
 // if (true) { console.log("HELLO WORLD!"); console.log("dwaDsa!") }
 import express from 'express';
+import http from 'http';
 import 'dotenv/config';
 
 const app = express();
 
-const port = process.env.PORT;
+const port = normalizePort(process.env.PORT);
+app.set('port', port);
 
 app.get('/', (req, res) => {
   res.json('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}!`);
-});
+const server = http.createServer(app);
+server.listen(port);
+server.on('listening', onListening);
+
+// app.listen(port, () => {
+//   console.log(`App listening on port ${port}!`);
+// });
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function normalizePort(val: any) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr?.port;
+  console.log('Listening on ' + bind);
+}
